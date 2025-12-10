d6e0f98aeb8e69f36ee1e7a4a0c6723fd12321a7
Janhavi Nazare <nazarejanhavi5@gmail.com>
2025-12-10 21:33:18 +0530

    gitignore file

---
+++ geometry.py
@@ -0,0 +1,5 @@
+def rect_area(a,b):
+    print(a*b)
+
+def rect_peri(a,b):
+    print(2*(a+b))
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import arithmetic
+import geometry
+
 
 arithmetic.add(3,7)
 arithmetic.sub(3,7)
+
+geometry.rect_area(3,7)
+geometry.rect_peri(3,7)
 print("Hello World")
